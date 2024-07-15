873387148bafb81c8ef3c4f67fc04d79cd96317b
Nithin1572 <122642750+Nithin1572@users.noreply.github.com>
2024-07-15 15:57:52 +0530

    Update UVLRM.py
    Added Z Scale Normalization

--- UVLRM.py
+++ UVLRM.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
 
+# class implementing univariate linear regression
 class UVLRM:
+
+    # initialize the values of paramenteres
     def __init__(self, w, b, alpha, numOfIterations):
         self.w = w
         self.b = b
         self.alpha = alpha
         self.numOfIterations = numOfIterations
 
+    # Cost function = J(w,b)
+    # Helps to know how well the model is performing
     def computeCost(self):
         cost = 0
         for i in range(0, self.m):
@@ -15,6 +20,8 @@ class UVLRM:
             cost = cost + (f_wb - self.Y[i])**2
         totalCost = cost / (2 * self.m)
     
+    # Calculated the gradient to update the actual parameters
+    # derivative of cost function w.r.t 'w' and 'b'   
     def computeGradient(self):
         dj_dw = 0
         dj_db = 0
@@ -26,25 +33,33 @@ class UVLRM:
         dj_db = dj_db / self.m
         return dj_dw, dj_db
 
+    # updates the actual parameters
     def batchGradientDescent(self):
         for i in range(0, self.numOfIterations):
             dj_dw, dj_db = self.computeGradient()
             self.w = self.w - self.alpha * dj_dw 
             self.b = self.b - self.alpha * dj_db
 
+    def Z_Scale_Normalization(self):
+        XMean = sum(self.X)/self.m
+        YMean = sum(self.Y)/self.m
+        XStandardDeviation = 0
+        YStandardDeviation = 0
+        for i in range(self.m):
+            XStandardDeviation += (self.X[i] - XMean)**2
+            YStandardDeviation += (self.Y[i] - YMean)**2
+        XStandardDeviation = np.sqrt(XStandardDeviation/self.m)
+        YStandardDeviation = np.sqrt(YStandardDeviation/self.m)
+        for i in range(self.m):
+            self.X[i] = (self.X[i] - XMean)/XStandardDeviation
+            self.Y[i] = (self.Y[i] - YMean)/YStandardDeviation
 
     def trainModel(self, fileName):
         trainData = pd.read_csv(fileName)
         self.m = len(trainData)
         self.X = trainData['x'].tolist()
         self.Y = trainData['y'].tolist()
-        maxElementX = max(self.X)
-        minElementX = min(self.X)
-        maxElementY = max(self.Y)
-        minElementY = min(self.Y)
-        for i in range(self.m):
-            self.X[i] = (self.X[i] - minElementX)/(maxElementX - minElementX)
-            self.Y[i] = (self.Y[i] - minElementY)/(maxElementY - minElementY)
+        self.Z_Scale_Normalization()
         self.batchGradientDescent()
     
     def predict(self, x):
